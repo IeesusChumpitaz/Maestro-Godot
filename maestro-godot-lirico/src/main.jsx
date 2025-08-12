b47f7985b49dfc7e38e5621d10debe21c6151c6f
Ieesus Chumpitaz <ichumpitaz17@gmail.com>
2025-08-11 20:43:59 -0500

    Fix: arreglando bugs de migracion a tailwind CSS

--- maestro-godot-lirico/src/main.jsx
+++ maestro-godot-lirico/src/main.jsx
@@ -2,19 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
-
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
